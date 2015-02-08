0407c8d99fef110cc165dcc795359907fc56108c
Palas Mullick <palasm@gmail.com>
2015-02-08 12:42:39 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -33,16 +33,18 @@ png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2))  
 
 #1-1  Global Active Power Plot 
-plot(convertedDateTime, convertedGlobalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2) 
+plot(convertedDateTime, convertedGlobalActivePower, type = "l", xlab = "", ylab = "Global Active Power", cex = 0.2) 
 
 # 1-2 Voltage Plot 
-plot(convertedDateTime, convertedVoltage, type="l", xlab="datetime", ylab="Voltage") 
+plot(convertedDateTime, convertedVoltage, type = "l", xlab = "datetime", ylab = "Voltage") 
 
 # 2-1 Energy Sub Metering  Plot 
-plot(convertedDateTime, convertedSubMetering1, type="l", ylab="Energy Sub metering", xlab="") 
-lines(convertedDateTime, convertedSubMetering2, type="l", col="red") 
-lines(convertedDateTime, convertedSubMetering3, type="l", col="blue") 
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="n") 
+plot(convertedDateTime, convertedSubMetering1, type = "l", ylab = "Energy Sub metering", xlab = "") 
+lines(convertedDateTime, convertedSubMetering2, type = "l", col = "red") 
+lines(convertedDateTime, convertedSubMetering3, type = "l", col = "blue") 
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty =, lwd = 2.5, col = c("black", "red", "blue"), bty = "n") 
 
 #2-2 Global Reactive Power Plot 
-plot(convertedDateTime, convertedGlobalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power") 
+plot(convertedDateTime, convertedGlobalReactivePower, type = "l", xlab = "datetime", ylab = "Global_reactive_power") 
+
+dev.off()
